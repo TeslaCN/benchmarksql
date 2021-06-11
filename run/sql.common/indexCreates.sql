53ede03b514675ea44a152b5717606d3d8be5806
justbk <249396768@qq.com>
2021-06-11 16:42:41 +0800

    modify index create sql

--- run/sql.common/indexCreates.sql
+++ run/sql.common/indexCreates.sql
@@ -13,8 +13,8 @@ create index bmsql_customer_idx1
 alter table bmsql_oorder add constraint bmsql_oorder_pkey
   primary key (o_w_id, o_d_id, o_id);
 
-create unique index bmsql_oorder_idx1
-  on  bmsql_oorder (o_w_id, o_d_id, o_carrier_id, o_id);
+create index bmsql_oorder_idx1
+  on  bmsql_oorder (o_w_id, o_d_id, o_c_id);
 
 alter table bmsql_new_order add constraint bmsql_new_order_pkey
   primary key (no_w_id, no_d_id, no_o_id);
